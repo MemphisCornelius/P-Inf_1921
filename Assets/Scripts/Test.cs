702dea0dc24d5ada25dac9ca32601d39170bfe8a
MemphisCornelius <theeasyminer@gmail.com>
2019-10-25 15:05:54 +0200

    Gegner

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Test : MonoBehaviour {
+    private Vector3 target;
+    public Transform player;
+    public float range;
+    public GameObject Wall;
+
+    public void PlaceWall ()
+    {
+        Vector3 difference = player.transform.position - transform.position;
+        float rotation = Mathf.Atan2(difference.y, difference.x);
+        float x = Mathf.Cos(rotation);
+        float y = Mathf.Sin(rotation);
+        Vector3 pos = new Vector3(x, y);
+        pos = pos * range + transform.position;
+        Instantiate(Wall, pos, Quaternion.Euler(0, 0, 0));
+    }
+}
---
+++ Assets/Scripts/Wall.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    Transform player;
+    float time = 5.0f;
+
+    void Awake()
+    {
+        player = GameObject.Find("Player").transform;
+        Vector3 difference = player.transform.position - transform.position;
+        float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ - 90);
+    }
+    void Update()
+    {
+        time -= Time.deltaTime;
+        if (time < 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
