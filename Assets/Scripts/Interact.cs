db9beddbfab3f23ce4e451f58f1443c89d471f18
AntonScheitler <anton.scheitler@gmail.com>
2019-11-08 15:23:07 +0100

    Interactionen_Fortführung

--- Assets/Scripts/Interact.cs
+++ Assets/Scripts/Interact.cs
@@ -1,14 +1,18 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Interact : MonoBehaviour
 {
     //Camera camera;
 
-    public GameObject interactebal;
+    public GameObject interactable;
     public float radius = 1.5f;
     private Transform player;
     private float distancex;
     private float distancey;
+    public bool Information;
+    public GameObject information_panel;
+    public GameObject information_text;
 
     private void Start()
     {
@@ -25,10 +29,19 @@ public class Interact : MonoBehaviour
     void Update()
     {
         Vector3 differenceplayer = player.transform.position - transform.position;
-        Vector3 differenceobject = interactebal.transform.position - transform.position;
+        Vector3 differenceobject = interactable.transform.position - transform.position;
         distancex = differenceplayer.x - differenceobject.x;
         distancey = differenceplayer.y - differenceobject.y;
         //Die Distanz auf der x- und y-Achse der beiden Objekte wird verglichen
+
+        if (distancex > radius)
+        {
+            if (distancey > radius)
+            {
+                information_panel.SetActive(false);
+                information_text.SetActive(false);
+            }
+        }
 
         if (Input.GetKeyDown("e"))
 
@@ -38,10 +51,18 @@ public class Interact : MonoBehaviour
                 if (distancey < radius)
                 {
                     Debug.Log("Es wurde interagiert");
+
+                    if (Information == true)
+                    {
+                        information_text.SetActive(true);
+                        information_panel.SetActive(true);
+                    }
                 }
             }
+            
             //Ist die Distanz zwischen den beiden Objekten kleiner als ein festgelegter Radius, so wird ein Debug ausgeführt
         }
+        
     }
 
 }
