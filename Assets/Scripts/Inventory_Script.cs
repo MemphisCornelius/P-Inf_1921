70827620ca80cacb7d0d020fe9358fc47839526d
AntonScheitler <anton.scheitler@gmail.com>
2019-12-08 12:15:30 +0100

    Inventory

---
+++ Assets/Scripts/Inventory_Script.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory_Script : MonoBehaviour
+{
+    public GameObject Inventory;
+
+    public void open()
+    {
+        Inventory.SetActive(true);
+    }
+
+    public void close()
+    {
+        Inventory.SetActive(false);
+    }
+}
