83470b0f3a6415f03806d3691f95e33fe590ccb1
Andi14-prog <57532716+Andi14-prog@users.noreply.github.com>
2019-12-28 15:54:11 +0100

    Writing Animation and Bed
    Writing Animation Script and Bed

---
+++ Assets/Scripts/WritingMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WritingMovement : MonoBehaviour
+{
+    //Zeit zum Erscheinen
+    public float letterPaused = 0.01f;
+    //Nachricht
+    public string message;
+    //Textcomponente
+    public Text textComp;
+
+    // Use this for initialization
+    void Start()
+    {
+        textComp = GetComponent<Text>();
+         message = textComp.text;
+        textComp.text = "";
+        StartCoroutine(TypeText());
+    }
+
+    IEnumerator TypeText()
+    {
+       
+        foreach (char letter in message.ToCharArray())
+        {
+            textComp.text += letter;
+            yield return 0;
+            yield return new WaitForSeconds(letterPaused);
+        }
+    }
+
+}
