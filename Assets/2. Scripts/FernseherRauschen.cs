32dbc05eca090834425e8c428a695efe508037c9
Aztharo <artur.zhyhaylo@gmail.com>
2020-01-06 21:14:51 +0100

    .

--- Assets/2. Scripts/FernseherRauschen.cs
+++ Assets/2. Scripts/FernseherRauschen.cs
@@ -32,6 +32,6 @@ public class FernseherRauschen : MonoBehaviour
         if (licht.activeInHierarchy && flackern)
         {
             fernseher.GetComponent<SpriteRenderer>().color = new Vector4(0, 0, 1, 1);
-        }
+        } 
     }
 }
