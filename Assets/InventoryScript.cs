0b81ec071e1dc9376a5b58488beec44102f9f593
Aztharo <artur.zhyhaylo@gmail.com>
2020-01-28 18:44:38 +0100

    .

--- Assets/InventoryScript.cs
+++ Assets/InventoryScript.cs
@@ -16,7 +16,7 @@ public class InventoryScript : MonoBehaviour
     }
     public void addToArray()
     {
-        items[zaehler] = new string[5];
+        items[zaehler] = ;
         zaehler++;
     }
     
