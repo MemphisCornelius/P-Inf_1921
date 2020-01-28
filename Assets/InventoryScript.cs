c584cf3b2f119e790c58d0430c124c39f16026c4
Aztharo <artur.zhyhaylo@gmail.com>
2020-01-28 18:43:37 +0100

    .

---
+++ Assets/InventoryScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryScript : MonoBehaviour
+{
+    public string[][] items = new string[10][];
+    //itemarray = name, descripton
+    public GameObject prefab, parent;
+
+    int zaehler = 0;
+
+    public void createSlot(GameObject prefab)
+    {
+        Instantiate(prefab, Vector3.zero, Quaternion.identity, parent.transform);
+    }
+    public void addToArray()
+    {
+        items[zaehler] = new string[5];
+        zaehler++;
+    }
+    
+    public void removeFromArray()
+    {
+
+    }
+}
