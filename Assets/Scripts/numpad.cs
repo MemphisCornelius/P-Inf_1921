c82eb2660c104ecd8f2395e9a6e7e61ac00f4fc0
marvin <theeasyminer@gmail.com>
2020-03-30 14:54:48 +0200

    $12o das numpad auf der tatstatur sollte jetzt auch gehen

--- Assets/Scripts/numpad.cs
+++ Assets/Scripts/numpad.cs
@@ -29,16 +29,16 @@ public class numpad : MonoBehaviour {
 
         if (numbpad.active)
         {
-            if(Input.GetKeyDown(KeyCode.Alpha0)) {TaskOnClick(0);}
-            if(Input.GetKeyDown(KeyCode.Alpha1)) {TaskOnClick(1);}
-            if(Input.GetKeyDown(KeyCode.Alpha2)) {TaskOnClick(2);}
-            if(Input.GetKeyDown(KeyCode.Alpha3)) {TaskOnClick(3);}
-            if(Input.GetKeyDown(KeyCode.Alpha4)) {TaskOnClick(4);}
-            if(Input.GetKeyDown(KeyCode.Alpha5)) {TaskOnClick(5);}
-            if(Input.GetKeyDown(KeyCode.Alpha6)) {TaskOnClick(6);}
-            if(Input.GetKeyDown(KeyCode.Alpha7)) {TaskOnClick(7);}
-            if(Input.GetKeyDown(KeyCode.Alpha8)) {TaskOnClick(8);}
-            if(Input.GetKeyDown(KeyCode.Alpha9)) {TaskOnClick(9);}
+            if(Input.GetKeyDown(KeyCode.Alpha0) || Input.GetKeyDown(KeyCode.Keypad0)) {TaskOnClick(0);}
+            if(Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(KeyCode.Keypad1)) {TaskOnClick(1);}
+            if(Input.GetKeyDown(KeyCode.Alpha2) || Input.GetKeyDown(KeyCode.Keypad2)) {TaskOnClick(2);}
+            if(Input.GetKeyDown(KeyCode.Alpha3) || Input.GetKeyDown(KeyCode.Keypad3)) {TaskOnClick(3);}
+            if(Input.GetKeyDown(KeyCode.Alpha4) || Input.GetKeyDown(KeyCode.Keypad4)) {TaskOnClick(4);}
+            if(Input.GetKeyDown(KeyCode.Alpha5) || Input.GetKeyDown(KeyCode.Keypad5)) {TaskOnClick(5);}
+            if(Input.GetKeyDown(KeyCode.Alpha6) || Input.GetKeyDown(KeyCode.Keypad6)) {TaskOnClick(6);}
+            if(Input.GetKeyDown(KeyCode.Alpha7) || Input.GetKeyDown(KeyCode.Keypad7)) {TaskOnClick(7);}
+            if(Input.GetKeyDown(KeyCode.Alpha8) || Input.GetKeyDown(KeyCode.Keypad8)) {TaskOnClick(8);}
+            if(Input.GetKeyDown(KeyCode.Alpha9) || Input.GetKeyDown(KeyCode.Keypad9)) {TaskOnClick(9);}
             if (Input.GetKeyDown(KeyCode.Delete)) {TaskOnClickDelete();}
 
         }
