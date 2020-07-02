54a4d371963d5b16b9569e52663d4f4347abba4f
AntonScheitler <anton.scheitler@gmail.com>
2020-07-02 20:21:29 +0200

    Level_1_Interactions
    Implementierung von Interaktionen für Level 1 wurden begonnen

---
+++ Assets/Interactions_Level_2.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Interactions_Level_2 : MonoBehaviour
+{
+    public GameObject Panel;
+    public bool Start_Riddle = false;
+    public Rigidbody2D Player;
+    public Vector2 distance;
+    public Rigidbody2D InteractObject;
+    public float Radius;
+
+    public void ClosePanel()
+    {
+        Panel.SetActive(false);
+    }
+
+
+    [System.Serializable] public class _UnityEventFloat : UnityEvent<GameObject> { }
+
+    public void Initiate_Riddle_Bookshelf()
+    {
+        distance = Player.position - InteractObject.position;
+        Radius = InteractObject.GetComponent<Interact>().radius;
+        if (distance.x < Radius && distance.y < Radius)
+        {
+            Panel.SetActive(true);
+            Start_Riddle = true;
+        }
+    }
+
+    public void Carpet_Interact()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
