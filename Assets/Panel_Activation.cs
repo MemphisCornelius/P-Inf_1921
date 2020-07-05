fdebf3093b65631912586ba82a73ec1c0c140473
AntonScheitler <anton.scheitler@gmail.com>
2020-07-05 19:36:57 +0200

    Robot1_Minigame
    Erstes Minispiel des 3. Levels designed und implementiert

--- Assets/Panel_Activation.cs
+++ Assets/Panel_Activation.cs
@@ -1,24 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Panel_Activation : MonoBehaviour
 {
     public GameObject EndGame_Panel;
     public GameObject Defeat_Text;
     public GameObject Victory_Text;
+    public GameObject[] RespawnedEnemies;
+    public float SpawnTime = 2;
+    public bool Respawned = false;
+    public int[] Scenes;
+    public int SceneToReturnTo;
 
     public void Update()
     {
         int Enemycount = GameObject.FindGameObjectsWithTag("Enemy").Length;
         int Playercount = GameObject.FindGameObjectsWithTag("Player").Length;
 
+        if (Enemycount <= 1 && Respawned != true)
+        {
+            SpawnTime = SpawnTime - Time.deltaTime;
+            if (SpawnTime <= 0)
+            {
+                foreach (GameObject RespawnedEnemy in RespawnedEnemies)
+                {
+                    RespawnedEnemy.SetActive(true);
+                    Respawned = true;
+                }
+            }
+        }
         if (Enemycount <= 0 || Playercount <= 0)
         {
             EndGame_Panel.SetActive(true);
             if (Enemycount <= 0)
             {
                 Victory_Text.SetActive(true);
+                SceneManager.LoadScene(SceneToReturnTo);
             }
             else
             {
