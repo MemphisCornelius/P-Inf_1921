64d0ff99b21f29180f8020b3ea75dd21e1bd2a3b
AntonScheitler <anton.scheitler@gmail.com>
2020-07-05 14:41:48 +0200

    Level_3_Interactions
    Alle Gegenstände des 3. Levels eingebaut
    Einige Interaktionen implementiert

---
+++ Assets/Interactions_Level_3.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class Interactions_Level_3 : MonoBehaviour
+{
+    public GameObject Panel;
+    public GameObject Information_Image;
+    public bool StartRiddle = false;
+
+    public void ClosePanel()
+    {
+        Panel.SetActive(false);
+        Information_Image.SetActive(false);
+    }
+
+    [System.Serializable] public class _UnityEventFloat : UnityEvent<GameObject> { }
+
+    public void DeskRiddle()
+    {
+        Panel.SetActive(true);
+        Information_Image.SetActive(true);
+        StartRiddle = true;
+    }
+
+    public void Robot_1_Interact()
+    {
+        if (StartRiddle)
+        {
+            SceneManager.LoadScene(3);
+        }
+    }
+
+    public void OpenDoor()
+    {
+        
+    }
+}
