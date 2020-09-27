db8369346a3727c402dfa3190d81bee4b3297d11
MemphisCornelius <39841089+MemphisCornelius@users.noreply.github.com>
2020-09-27 21:09:11 +0200

    Fixed Bugs in lvl 2 which disallowed to interact and added textures to the dir

---
+++ Assets/Perspective.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Perspective : MonoBehaviour {
+    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject g;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log(other);
+    }
+}
