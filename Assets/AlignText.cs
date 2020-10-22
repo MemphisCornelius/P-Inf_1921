f245c189af54e6de1d76796b50739018a36b0ecf
AntonScheitler <anton.scheitler@gmail.com>
2020-10-22 16:18:37 +0200

    MatheShooter_fertig
    Mathe_Shooter fertiggestellt und in jedem Level implementiert (Rätsel müssen eingefügt werden)

---
+++ Assets/AlignText.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AlignText : MonoBehaviour
+{
+    public GameObject enemyObject;
+    public bool respawnEnemy = false;
+
+    void Update()
+    {
+        Vector3 textPos = Camera.main.WorldToScreenPoint(enemyObject.transform.position);
+        Vector3 replaceVec = new Vector3(900, 900, 900);
+        if (!respawnEnemy)
+        {
+            transform.position = textPos;
+        }
+        else
+        {
+            transform.position = replaceVec;
+        }
+        if (enemyObject.activeSelf)
+        {
+            respawnEnemy = false;
+        }
+        if (!enemyObject.activeSelf && !respawnEnemy)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
