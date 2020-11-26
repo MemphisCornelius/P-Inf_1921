2ed1ed78b7d10139643af5fa762dd4242abddcbb
MemphisCornelius <39841089+MemphisCornelius@users.noreply.github.com>
2020-11-26 16:02:18 +0100

    Notizbuch Szenenübergreifend gemacht und Notizbuchbeispiel implementation [Artur]

--- Assets/Scripts/Notizbuch/Page.cs
+++ Assets/Scripts/Notizbuch/Page.cs
@@ -4,6 +4,11 @@ class Page : MonoBehaviour{
     public bool found = false;
     public GameObject sprite;
 		
+public void Awake() {
+		if(PlayerPrefs.GetInt("Page" + page) == 1) {
+			found = true;		
+		}
+	}
     public void SetFound() {
         found = true;
         PlayerPrefs.SetInt("Page" + page, 1);
